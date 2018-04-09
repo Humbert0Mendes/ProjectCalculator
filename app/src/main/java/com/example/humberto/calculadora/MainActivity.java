d2fbacb9c57086c362cee9b517b5dfa2263f660a
Humbert0Mendes <hmpn87@gmail.com>
2018-04-09 14:06:50 -0300

    Criação do layout, inicio do desenvolvimento da rotina.

--- app/src/main/java/com/example/humberto/calculadora/MainActivity.java
+++ app/src/main/java/com/example/humberto/calculadora/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.humberto.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operacao;
+    private TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        operacao = (EditText)findViewById(R.id.cpOperacao);
+        result = (TextView)findViewById(R.id.resultado);
+
+    }
+
+    public void calculaOperacao(View view){
+        String vlOperacao = operacao.getText().toString();
+        float valor = Float.parseFloat(vlOperacao);
+
+
+    }
+
+    public void botao_zero (View view){
+
+        operacao.setText(operacao.getText().toString() + "0");
+    }
+
+    public  void botao_pt (View view){
+        operacao.setText(operacao.getText().toString() + ".");
+    }
+
+    public void botao_um(View view) {
+        operacao.setText(operacao.getText().toString() + "1");
     }
 }
