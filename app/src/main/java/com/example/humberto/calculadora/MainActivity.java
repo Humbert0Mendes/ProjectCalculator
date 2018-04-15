5cd1dddd41fcbb4b2f8d041a0d73035044e5c4ab
Humbert0Mendes <hmpn87@gmail.com>
2018-04-15 00:55:44 -0300

    hotfix/Calculator-1

--- app/src/main/java/com/example/humberto/calculadora/MainActivity.java
+++ app/src/main/java/com/example/humberto/calculadora/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             dlg.setTitle("Aviso!");
             dlg.setMessage("Operação inválida, tente novamente!");
             dlg.show();
+            
         }
     }
 
