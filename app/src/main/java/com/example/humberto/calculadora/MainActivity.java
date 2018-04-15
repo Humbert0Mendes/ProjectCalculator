599df77a5d1c81a151501495103aa009827094ff
Humbert0Mendes <hmpn87@gmail.com>
2018-04-15 00:55:23 -0300

    hotfix/Calculator-1

--- app/src/main/java/com/example/humberto/calculadora/MainActivity.java
+++ app/src/main/java/com/example/humberto/calculadora/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.humberto.calculadora;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,14 +23,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calculaOperacao(View view){
+        String resultadoFim = null;
+        result.setText("");
         String vlOperacao = operacao.getText().toString();
-        float valor = Float.parseFloat(vlOperacao);
+        String[] vl;
+        vl = vlOperacao.split("[-+/*]");
+        String vTeste = vl[0];
+        String vTeste2 = vl[1];
+        System.out.println("Valor teste: " + vTeste + " Valor teste 2: " +vTeste2);
 
+        try {
+            if (vlOperacao.contains("+")) {
+                resultadoFim = vTeste + vTeste2;
+                result.setText("Resultado: " + resultadoFim);
 
+            } else if (vlOperacao.contains("-")) {
+                Float fl = Float.parseFloat(vTeste);
+                Float fl2 = Float.parseFloat(vTeste2);
+                Float fl3 = fl - fl2;
+                resultadoFim = String.valueOf(fl3);
+                result.setText("Resultado: " + resultadoFim);
+
+            } else if (vlOperacao.contains("*")) {
+                Float multi1 = Float.parseFloat(vTeste);
+                Float multi2 = Float.parseFloat(vTeste2);
+                Float multiVl = multi1 * multi2;
+                resultadoFim = String.valueOf(multiVl);
+                result.setText("Resultado: " + resultadoFim);
+            }
+        }catch (Exception e){
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("Aviso!");
+            dlg.setMessage("Operação inválida, tente novamente!");
+            dlg.show();
+        }
     }
 
+    //Implementação das ações dos botões!
     public void botao_zero (View view){
-
         operacao.setText(operacao.getText().toString() + "0");
     }
 
@@ -40,4 +71,56 @@ public class MainActivity extends AppCompatActivity {
     public void botao_um(View view) {
         operacao.setText(operacao.getText().toString() + "1");
     }
+
+    public void botao_dois(View view) {
+        operacao.setText(operacao.getText().toString() + "2");
+    }
+
+    public void botao_tres(View view) {
+        operacao.setText(operacao.getText().toString() + "3");
+    }
+
+    public void botao_quatro(View view) {
+        operacao.setText(operacao.getText().toString() + "4");
+    }
+
+    public void botao_cinco(View view) {
+        operacao.setText(operacao.getText().toString() + "5");
+    }
+
+    public void botao_seis(View view) {
+        operacao.setText(operacao.getText().toString() + "6");
+    }
+
+    public void botao_sete(View view) {
+        operacao.setText(operacao.getText().toString() + "7");
+    }
+
+    public void botao_oito(View view) {
+        operacao.setText(operacao.getText().toString() + "8");
+    }
+
+    public void botao_nove(View view) {
+        operacao.setText(operacao.getText().toString() + "9");
+    }
+
+    public void botao_soma(View view) {
+        operacao.setText(operacao.getText().toString() + "+");
+    }
+
+    public void botao_sub(View view){ operacao.setText(operacao.getText().toString() + "-");}
+
+    public void botao_multi(View view){operacao.setText(operacao.getText().toString() + "*");}
+
+    public void botao_ce (View view) {operacao.setText("");}
+
+    public void botao_c(View view){
+            String valorCampo = operacao.getText().toString();
+            if(valorCampo.length()>=1){
+                String newValorCampo = valorCampo.substring(0,valorCampo.length()-1);
+                operacao.setText(newValorCampo);
+            }
+
+        }
+
 }
